d2f7b2b3eeaab20516ec29b8a5b3c4675217c6af
Rohit Suryawanshi <47253310+suryaadev@users.noreply.github.com>
2023-03-17 23:36:28 +0530

    Update user.js

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -6,7 +6,9 @@ const router = express.Router();
 import { UserModel } from "../models/Users.js";
 
 router.post("/register", async (req, res) => {
+  
   const { username, password } = req.body;
+  
   const user = await UserModel.findOne({ username });
   if (user) {
     return res.status(400).json({ message: "Username already exists" });
